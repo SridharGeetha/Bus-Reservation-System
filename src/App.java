200f78423ea8d6f8d7cd06e97894a375271789df
sridhargeetha <ssridhar4858@gmail.com>
2025-06-14 16:36:24 +0530

    bus reservetion code

--- src/App.java
+++ src/App.java
@@ -111,14 +111,28 @@ class Bus implements ListInformation{
     }
     @Override
     public void listInformation() {
-        System.out.println("Bus Name : "+busName+" Capacity : "+capacity);
+        System.out.println("Bus Id : "+busId+" Bus Name : "+busName+"   Capacity : "+capacity);
     }
 }
 class Booking implements ListInformation{
     private String userName;
     private int busNo;
+    private String busName;
     private Date date; 
+    
+    public Booking(String userName, int userId, int busNo, String busName, Date date) {
+        this.userName = userName;
+        this.busNo = busNo;
+        this.busName = busName;
+        this.date = date;
+    }
 
+    public String getBusName() {
+        return busName;
+    }
+    public void setBusName(String busName) {
+        this.busName = busName;
+    }
     
     public Booking(String userName, int busNo, Date date) {
         this.userName = userName;
@@ -143,6 +157,28 @@ class Booking implements ListInformation{
     public Date getDate() {
         return date;
     }
+
+    public static void busBooking(int busId,ArrayList<Bus> bus,ArrayList<Booking> bookings,String name,int ticket
+    ){
+        for(Bus b : bus){
+            if(b.getBusId() == busId){
+                if(b.getCapacity()>=ticket){
+                    Booking newBooking = new Booking(name, b.getBusId(), new Date());
+                    bookings.add(newBooking);
+                    b.setCapacity(b.getCapacity()-ticket);
+                    System.out.println("Bus Booked Successfully");
+                    return;
+                }else{
+                    System.out.println(ticket-b.getCapacity()+" Not Available");
+                    return;
+                }
+            }
+            else{
+                System.out.println("Not Found Bus for Given bus ID");
+            }
+            
+        }
+    }
     @Override
     public void listInformation() {
 
@@ -154,6 +190,14 @@ public class App {
        ArrayList<Customer> customers = new ArrayList<>();
        customers.add(new Customer(1,"Sridhar","sridhar123@gmail.com","sri123@@"));
        customers.add(new Customer(2,"sam","sam123@gmail.com","sam123@@"));
+       customers.add(new Customer(3,"s","s","s"));
+       ArrayList<Bus> bus = new ArrayList<>();
+       bus.add(new Bus(1, "Rathimeena", true, 2));
+       bus.add(new Bus(2, "SR Travels", true, 50));
+       bus.add(new Bus(3, "Maioe Travels", true, 50));
+       bus.add(new Bus(4, "Ken Travels", true, 50));
+       ArrayList<Booking> bookings = new ArrayList<>();
+
        while (true) {
         System.out.println("1.Login");
         System.out.println("2.Exit");
@@ -167,12 +211,44 @@ public class App {
                 String password = sc.nextLine();
                 boolean isLogin = Customer.Login(customers, email, password);
                 while(isLogin) {
-                    
+                    System.out.println("\n------Welcome to Bus Reservation System------\n");
+                    System.out.println("1.Available Bus");
+                    System.out.println("2.Reserve Bus");
+                    System.out.println("3.Logout");
+                    int op = sc.nextInt();
+                    switch (op) {
+                        case 1:
+                            for(Bus b : bus){
+                                if(b.getCapacity()>0)
+                                b.listInformation();
+                            }
+                            break;
+                        case 2:
+                            for(Bus b : bus){
+                                b.listInformation();
+                            }
+                            System.out.print("Select Bus Id: ");
+                            int busId = sc.nextInt();
+                            System.out.println("Please Provide Your Name : ");
+                            sc.next();
+                            String name = sc.nextLine();
+                            System.out.println("Please Provide Your Ticket : ");
+                            int ticket = sc.nextInt();
+                            Booking.busBooking(busId, bus,bookings, name,ticket);
+                            break;
+                        case 3:
+                            isLogin = false;
+                            break;
+                        default:
+                        System.out.println("Invalid Option Please enter valid one");
+                            break;
+                    }
+
                 }
                 break;
         
             default:
-                System.out.println("hello");
+                System.exit(0);
                 break;
         }
        }
